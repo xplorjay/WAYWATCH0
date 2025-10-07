1d413edb6f14799c102985f453103962e042f25c
jay <xplorerjay@gmail.com>
2025-10-07 22:00:49 +0530

    changed readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # waywatch0
 
 A new Flutter project.
+
+Assets are in build/native_assets
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() {
   runApp(const WayWatchApp());
